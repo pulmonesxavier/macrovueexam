97d1f3f453ccd7bc7f7ca0b0574443d1f002cb6c
pulmonesxavier <xavierluke@Xaviers-MacBook-Pro.local>
2021-08-24 04:26:59 +0800

    test: Added user creation test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from unittest import expectedFailure
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class BaseTest(APITestCase):
+    def setUp(self):
+
+        self.base_user = User.objects.create_user('macrovue', 'test@test.com', 'pass123')
+
+    @expectedFailure
+    def test_create_user(self):
+        """
+        Test a successful user creation.
+        """
+        data = {
+            'username': 'xavier',
+            'email': 'xavier@xavier.com',
+            'password': 'xavierpass'
+        }
+
+        res = self.client.post('api/auth/', data=data, format='json')
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(res.data['email'], data['email'])
+        self.assertIsNotNone(User.objects.get(email='xavier@xavier.com'))
