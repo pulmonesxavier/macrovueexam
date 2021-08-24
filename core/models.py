93d5330117d8d7a7837d254f92b7dfe73b961124
pulmonesxavier <xavierluke@Xaviers-MacBook-Pro.local>
2021-08-24 15:11:47 +0800

    feat: Added Stock model with migration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+    """
+    Stock model that holds the necessary information for a specific stock
+    """
+    name = models.CharField(max_length=50, blank=False)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
