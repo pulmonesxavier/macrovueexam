a56fafa6717b2c18062105252f7ce269a3569e32
pulmonesxavier <xavierluke@Xaviers-MacBook-Pro.local>
2021-08-25 09:58:05 +0800

    feat: Created a model for Order

--- core/models.py
+++ core/models.py
@@ -13,3 +13,16 @@ class Stock(models.Model):
         return self.name
 
 
+class Order(models.Model):
+    """
+    Order model that summarizes information on user orders
+    """
+    TYPE_CHOICES = [
+        (1, 'BUY'),
+        (2, 'SELL'),
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    type = models.BooleanField(choices=TYPE_CHOICES, blank=False) 
+    quantity = models.PositiveIntegerField(default=0)
+
